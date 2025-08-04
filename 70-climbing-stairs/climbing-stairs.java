e93d803091f6e6fe1f509dc0f5387a09479caf94
Karthikeyan <karthiknashokkumar@gmail.com>
2025-08-04 15:22:34 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (50.55%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -4,13 +4,13 @@ class Solution {
         {
             return n;
         }
-        int a[] = new int[n];
-        a[0] = 1;
-        a[1] = 2;
-        for(int i=2;i<n;i++)
+        int a[] = new int[n+1];
+        a[1] = 1;
+        a[2] = 2;
+        for(int i=3;i<=n;i++)
         {
             a[i] = a[i-1]+a[i-2];
         }
-        return a[n-1];
+        return a[n];
     }
 }
