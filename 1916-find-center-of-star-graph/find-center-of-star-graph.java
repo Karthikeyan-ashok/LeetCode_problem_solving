f7f383c857a66bab98daacfa1e4738d418051de1
Karthikeyan <karthiknashokkumar@gmail.com>
2025-08-05 13:47:25 +0530

    Time: 0 ms (100.00%) | Memory: 72.3 MB (53.09%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -1,24 +1,34 @@
 class Solution {
     public int findCenter(int[][] edges) {
-        Map<Integer,List<Integer>> map = new HashMap<>();
+        // Map<Integer,List<Integer>> map = new HashMap<>();
 
-        for(int i=0;i<edges.length;i++)
-        {
-            map.putIfAbsent(edges[i][0],new ArrayList<>());
-            map.putIfAbsent(edges[i][1],new ArrayList<>());
+        // for(int i=0;i<edges.length;i++)
+        // {
+        //     map.putIfAbsent(edges[i][0],new ArrayList<>());
+        //     map.putIfAbsent(edges[i][1],new ArrayList<>());
 
-            map.get(edges[i][0]).add(edges[i][1]);
-            map.get(edges[i][1]).add(edges[i][0]);
-        }
-        int n = 0;
-        for(int i : map.keySet())
+        //     map.get(edges[i][0]).add(edges[i][1]);
+        //     map.get(edges[i][1]).add(edges[i][0]);
+        // }
+        // int n = 0;
+        // for(int i : map.keySet())
+        // {
+        //     if(map.get(i).size()==edges.length)
+        //     {
+        //         n = i;
+        //         break;
+        //     }
+        // }
+        // return n;
+        int[]a = edges[0];
+        int[]b = edges[1];
+        if(a[0]==b[0] || a[0]==b[1])
         {
-            if(map.get(i).size()==edges.length)
-            {
-                n = i;
-                break;
-            }
+            return a[0];
         }
-        return n;
+        else
+        {
+            return a[1];
+        }
     }
 }
