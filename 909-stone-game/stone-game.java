512503643b37b11d9304e44902d77d116afae59d
Karthikeyan <karthiknashokkumar@gmail.com>
2025-08-07 18:10:18 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (81.01%) - LeetSync

---
+++ 909-stone-game/stone-game.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int total = 0;
+        for(int i : piles)
+        {
+            total += i;
+        }
+        int frontSum = 0;
+        for(int i=0;i<piles.length;i+=2)
+        {
+            frontSum += piles[i];
+        }
+
+        int backSum = 0;
+        for(int i=piles.length-1;i>=0;i-=2)
+        {
+            backSum += piles[i];
+        }
+
+        return frontSum>total-frontSum || backSum>total-backSum;
+    }
+}
