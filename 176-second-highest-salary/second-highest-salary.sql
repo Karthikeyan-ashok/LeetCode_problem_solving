7bd603316bf31af7265f496e538067d8c886d9ec
Karthikeyan <karthiknashokkumar@gmail.com>
2025-08-09 18:53:52 +0530

    Time: 272 ms (53.91%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,7 +1,6 @@
 select(
-    select salary
-    from employee
-    group by salary
+    select distinct salary 
+    from Employee
     order by salary desc
     limit 1 offset 1
 ) as SecondHighestSalary;
