0baef99e1195d20c50bc1ef8cc7ee2427ac6fbc1
Karthikeyan <karthiknashokkumar@gmail.com>
2025-08-12 09:21:32 +0530

    Time: 63 ms (5.09%) | Memory: 47.2 MB (5.18%) - LeetSync

--- 449-serialize-and-deserialize-bst/serialize-and-deserialize-bst.java
+++ 449-serialize-and-deserialize-bst/serialize-and-deserialize-bst.java
@@ -9,34 +9,41 @@
  */
 public class Codec {
 
-    static String data; 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        if(root==null)return "";
+        if(root==null)
+        {
+            return "";
+        }
         Queue<TreeNode> queue=new LinkedList<>();
         queue.offer(root);
-        String ans=root.val+" ";
-        while(!queue.isEmpty()){
+        String ans = root.val+" ";
 
-            int size=queue.size();
-            for(int i=0;i<size;i++){
-                TreeNode node=queue.poll();
-                //data+=node.val+" ";
-               
-                if(node.left!=null){
-                 queue.offer(node.left);
-                 ans+=node.left.val+" ";
-                 }else{
-                    ans+="null ";
+        while(!queue.isEmpty())
+        {
+            int size = queue.size();
+            for(int i=0;i<size;i++) 
+            {
+                TreeNode node = queue.poll();
+                if(node.left!=null)
+                {
+                    queue.offer(node.left);
+                    ans+=node.left.val+" ";
+                 }
+                 else
+                 {
+                    ans += "null ";
                  }
 
-                if(node.right!=null){
+                if(node.right!=null)
+                {
                     queue.offer(node.right);
                     ans+=node.right.val+" ";
-                }else{
-                    ans+="null ";
-                 }
-                 
+                }
+                else
+                {
+                    ans += "null ";
+                } 
             }
         }
         return ans;
@@ -46,21 +53,26 @@ public class Codec {
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
         if(data.equals(""))
-        return null;
-       Queue<TreeNode> q=new LinkedList<>();
+        {
+            return null;
+        }
 
+        Queue<TreeNode> q=new LinkedList<>();
         String[] arr=data.split(" ");
         TreeNode node=new TreeNode(Integer.parseInt(arr[0]));
         q.add(node);
-        int i=1;
-        while(i<arr.length){
-            TreeNode root=q.poll();
-            if(!arr[i].equals("null") && i<arr.length){
+        int i = 1;
+        while(i<arr.length)
+        {
+            TreeNode root = q.poll();
+            if(!arr[i].equals("null") && i<arr.length)
+            {
                 root.left=new TreeNode(Integer.parseInt(arr[i]));
                 q.add(root.left);
             }
             i++;
-            if(!arr[i].equals("null") && i<arr.length){
+            if(!arr[i].equals("null") && i<arr.length)
+            {
                 root.right=new TreeNode(Integer.parseInt(arr[i]));
                 q.add(root.right);
             }
