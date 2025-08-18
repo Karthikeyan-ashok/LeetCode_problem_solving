72d0727ad6f51c94bfeb34884ebd3359c46f8e25
Karthikeyan <karthiknashokkumar@gmail.com>
2025-08-18 16:08:02 +0530

    Time: 6 ms (73.69%) | Memory: 43.8 MB (42.05%) - LeetSync

---
+++ 494-target-sum/target-sum.java
@@ -0,0 +1,44 @@
+class Solution {
+    static int [][]dp;
+    
+    public static int subSetSum(int [] nums, int sum){
+        int n = nums.length;
+        dp = new int[n+1][sum+1];
+
+        for(int i = 0; i <n+1; i++){
+            dp[i][0] = 1;
+        }
+
+        for(int i=1; i<n+1; i++)
+        {
+            for(int j=0; j<sum+1; j++)
+            {
+                if(nums[i-1] <= j)
+                {
+                    dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
+                } 
+                else 
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for (int num : nums) 
+        {
+            sum += num;
+        }
+
+        if (sum < Math.abs(target) || (target + sum) % 2 != 0)
+        {
+            return 0;
+        }
+
+        int s1 = (target + sum) / 2;
+        return subSetSum(nums, s1);  
+    }
+}
